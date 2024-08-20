2ea1ed0abca530b59433a56f6868e170a169af29
guojiawei <2906498725@qq.com>
2024-08-20 13:25:18 +0800

    dev新增

---
+++ Dev.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: Mr.g
+ * @Description: TODO
+ * @DateTime: 2024/8/20 13:24
+ **/
+public class Dev {
+    public static void main(String[] args) {
+
+        System.out.println("dev");
+    }
+}
